6a1ce304b999a8ab6fc7a18a759369105a93e678
Ayodele Afolabi <105075157+deleayo535@users.noreply.github.com>
2025-02-10 09:52:46 +0100

    production error fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-"use client";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ClientWrapper from "./ClientWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,15 +21,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ClientWrapper children={children}>
         {children}
+        </ClientWrapper>
       </body>
     </html>
   );
